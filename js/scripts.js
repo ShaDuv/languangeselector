02d7a136b6cb521de2d80e5d58c18acaaf2b9ea4
Shawna Duvall <shawnabduvall@gmail.com>
2019-06-07 13:54:05 -0700

    adds more js

--- js/scripts.js
+++ js/scripts.js
@@ -1,15 +1,20 @@
 $(document).ready(function (e) {
       console.log('ready');
-      var answers = $('.input').serializeArray();
-      var scores = {
-        ruby: 0,
-        python: 0,
-        javascript: 0,
-      };
-      for (var answer of answers) {
-        scores[answer.value] += 1;}
-      console.log(scores);
-      for (var program in answer) {
-        console.log(scores[program]);
-      }
+      $("button").click(function(){
+          event.preventDefault();
+          var answers = $('.input').serializeArray();
+          var scores = {
+            ruby: 0,
+            python: 0,
+            javascript: 0,
+          };
+          for (var answer of answers) {
+            scores[answer.value] += 1;
+            console.log(answer.value);
+          } console.log(scores);
+
+          if (scores[python] >= scores[ruby] && scores[python] >= scores[javascript]) {
+            return 'Python';
+          }
+      });
     });
