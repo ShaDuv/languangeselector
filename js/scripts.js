5bc535a8d6ac0c726bd5cbc1f0f5fc661f17da65
Shawna Duvall <shawnabduvall@gmail.com>
2019-06-07 12:41:43 -0700

    adds first part of function

--- js/scripts.js
+++ js/scripts.js
@@ -1,2 +1,15 @@
-$(document).ready(function (event) {
-  });
+$(document).ready(function (e) {
+      console.log('ready');
+      var answers = $(':input').serializeArray();
+      var scores = {
+        ruby: 0,
+        python: 0,
+        javascript: 0,
+      };
+      for (var answer of answers) {
+        scores[answer.value] += 1;}
+      console.log(scores);
+      for (var program in answer) {
+        console.log(scores[program]);
+      }
+    });
