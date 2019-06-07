688ac31d5d894ad0a7103261a72491767ebc4a58
Shawna Duvall <shawnabduvall@gmail.com>
2019-06-07 11:28:38 -0700

    troubleshoots jquery not working

--- js/scripts.js
+++ js/scripts.js
@@ -1,7 +1,3 @@
 $(document).ready(function () {
-    $('#quiz').submit(function (event) {
-      event.preventDefault();
-      var answers = $(this).seriaolizeArray();
-      console.log(answers);
-    });
+  //your code here
   });
